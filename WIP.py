b5eff80cae9938c2c95101080b944ee515886179
Ashton <tylhandrias@gmail.com>
2012-04-21 16:57:05 -0400

    Windows support added.

--- WIP.py
+++ WIP.py
@@ -1,4 +1,10 @@
-import curses
+curses=False
+"""try:
+    import curses
+    curses=True
+except ImportError:
+    pass"""
+import sys
 
 class World:
     def __init__(self):
@@ -8,8 +14,8 @@ class World:
         vine = Interactable(("vine", "vines", "ivy"), self.tutorialship.room1, "asdf")
         door_mem = Memory(("door", "exit", "doors", "exit doors", "exit door"), "The blast doors are, somewhow, secure.\nThey were originally meant to ensure\nthat nobody got in. It seems that you're\nnot getting out - of that you're sure.\n")
         room_mem = Memory(("bustle", "room", "here"), "This room used to be constantly busy - \npeople used to be in and out constantly,\nchecking your vitals, diagnosing, tweaking,\nand always, constantly, worrying, fretting.\n")
-        meta_mem = Memory(("rocket", "December", "memory"), "You don't know why you think the way\nthat you do - you associate the array\nof things that you do with strange words.\nYou blame the hordes of clipboard nerds.\n")
-        nerd_mem = Memory(("nerds", "hordes", "nerd", "horde", "clipboard", "clipboards"), "They built you, you know. They put you\ntogether, bit by bit, \nscrew by screw.\nI would know - I was one of the nerds\nthat threw you together from broken words.\n")
+        meta_mem = Memory(("rocket", "december", "memory"), "You don't know why you think the way\nthat you do - you associate the array\nof things that you do with strange words.\nYou blame the hordes of clipboard nerds.\n")
+        nerd_mem = Memory(("nerds", "hordes", "nerd", "horde", "clipboard", "clipboards"), "They built you, you know. They put you\ntogether, bit by bit, screw by screw.\nI would know - I was one of the nerds\nthat threw you together from broken words.\n")
         for memory in (door_mem, meta_mem, nerd_mem, room_mem):
             self.memoryList.append(memory)
 
@@ -62,13 +68,18 @@ class Player(Agent):
     def __init__(self, world):
         self.world = world
     def act(self, userInput=""):
-        userInput = userInput.decode().lower()
+        if(curses==True):
+            userInput = userInput.decode().lower()
+        else:
+            userInput = userInput.lower()
         inputList = userInput.split(" ")
         if (inputList[0]=="quit"):
             curses.nocbreak()
             curses.echo()
             curses.endwin()
-            exit("A clean exit.")
+            sys.exit()
+        elif userInput == "crash":
+            raise TypeError
         elif userInput == "remember" or inputList[0] == "remember":
             return "You don't know how to \"remember\".\nYou do know how to \"rocket\", however."
         elif userInput == "rocket":
--- setup.py
+++ setup.py
@@ -8,16 +8,21 @@ world1 = World()
 #print elem
 
 #import and initialize curses
-import curses
-global stdscr
-stdscr = curses.initscr()
-stdscr.keypad(1)
-curses.nocbreak()
-curses.echo()
+curses=False
+"""try:
+    import curses
+    curses=True
+    global stdscr
+    stdscr = curses.initscr()
+    stdscr.keypad(1)
+    curses.nocbreak()
+    curses.echo()
+except ImportError:
+    pass"""
+import sys
 
-def inputParse():
-    """Redraw the curses screen and get user input, returned as a string."""
 
+def curses_inputParse():
     while(True):
         stdscr.addstr(curses.LINES-1, 0, ">")
         c = ''
@@ -25,16 +30,25 @@ def inputParse():
         stdscr.clear()
         stdscr.refresh()        
         return c
-
-stdscr.addstr(0, 0, "Hello. You don't wake up; you come to.\nThat's an important distinction to you.\nYou should look around. You remember\nthat \"look\" is bound to \"December.\"\n")
-while(True):
-    try:
-        stdscr.addstr(0, 0, world1.player1.act(inputParse()))
+if(curses==True):
+    stdscr.addstr(0, 0, "Hello. You don't wake up; you come to.\nThat's an important distinction to you.\nYou should look around. You remember\nthat \"look\" is bound to \"December.\"\n")
+    while(True):
+        try:
+            stdscr.addstr(0, 0, world1.player1.act(curses_inputParse()))
     #except curses.error:
         #pass
+        except Exception as e:
+            curses.nocbreak()
+            curses.echo()
+            curses.endwin()
+        #exit("Oops! We broke something.")
+            raise e
+
+#def plain_inputParse():
+print("Hello. You don't wake up; you come to.\nThat's an important distinction to you.\nYou should look around. You remember\nthat \"look\" is bound to \"December.\"")
+while(True):
+    try:
+        print(world1.player1.act(input(">")).rstrip())
     except Exception as e:
-        curses.nocbreak()
-        curses.echo()
-        curses.endwin()
-        exit("Oops! We broke something.")
         raise e
+        
